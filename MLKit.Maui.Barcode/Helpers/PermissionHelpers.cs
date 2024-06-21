0caaae1ad85b87535bd6bb6279dd7c6d6da81a32
Jake <jakee2000jake@gmail.com>
2024-06-20 19:27:47 -0500

    Move over barcode

---
+++ MLKit.Maui.Barcode/Helpers/PermissionHelpers.cs
@@ -0,0 +1,20 @@
+﻿namespace MLKit.Maui.Barcode;
+
+/// <summary>
+/// Helper methods for <see cref="Permissions"/>
+/// </summary>
+public class PermissionHelpers
+{
+    /// <summary>
+    /// Requests the necessary permissions for <see cref="IBarcodeService"/>
+    /// </summary>
+    /// <remarks>Required Permissions: <see cref="Permissions.StorageRead"/></remarks>
+    public static async Task<PermissionStatus> RequestBarcodeServicePermissions()
+    {
+        var status = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+        if (status != PermissionStatus.Granted)
+            status = await Permissions.RequestAsync<Permissions.StorageRead>();
+
+        return status;
+    }
+}
---
+++ MLKit.Maui.Barcode/Platforms/iOS/BarcodeService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MLKit.Maui.Barcode;
+
+public class BarcodeService : IBarcodeService
+{
+    public Task<List<BarcodeResult>> GetBarcodesFromImage(FileResult imageFile)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<BarcodeResult>> GetBarcodesFromImage(byte[] imageBytes)
+    {
+        throw new NotImplementedException();
+    }
+}
